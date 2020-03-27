3b8d8fe849fa14776c844b35413def94e70ec291
eo39 <zhekaonichsenko@gmail.com>
2020-03-27 21:06:46 +0200

    v0.1
    Заготовка формы

---
+++ Osmos/MainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Osmos
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameField = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameField
+            // 
+            this.gameField.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gameField.Location = new System.Drawing.Point(12, 12);
+            this.gameField.Name = "gameField";
+            this.gameField.Size = new System.Drawing.Size(1542, 720);
+            this.gameField.TabIndex = 0;
+            this.gameField.TabStop = false;
+            this.gameField.Paint += new System.Windows.Forms.PaintEventHandler(this.Draw);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1566, 744);
+            this.Controls.Add(this.gameField);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Osmos";
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox gameField;
+    }
+}
+
---
+++ Osmos/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Osmos
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            gameField.Paint += Draw;
+        }
+
+        private void Draw(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+        }
+    }
+}
