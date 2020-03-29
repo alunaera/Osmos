1e2cceb263b0cef5a2659e77a765368a2ece5c43
eo39 <zhekaonichsenko@gmail.com>
2020-03-29 16:58:21 +0200

    v1b
    - Добавлены новые методы в класс GameObject

---
+++ Osmos/GameObjects/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+namespace Osmos
+{
+    internal abstract class GameObject
+    {
+        public int PositionX { get; protected set; }
+        public int PositionY { get; protected set; }
+        public int VectorX { get; protected set; }
+        public int VectorY { get; protected set; }
+        public int Radius { get; protected set; }
+
+        public double Area => Radius * Radius * Math.PI;
+
+        public void Update()
+        {
+            PositionX += VectorX;
+            PositionY += VectorY;
+        }
+
+        public void Draw(Graphics graphics, Brush brush)
+        {
+            graphics.FillEllipse(brush, PositionX, PositionY, Radius, Radius);
+        }
+    }
+}
